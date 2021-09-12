9af63a1b19706ac4e734353845fe736949d1d77d
slpwlkr <shadezmax41@gmail.com>
2021-09-12 20:52:34 +0800

    add jwt auth function

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,14 @@
 import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
 import { LocalAuthGuard } from './auth/local-auth.guard';
+import { AuthService } from './auth/auth.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly authService: AuthService
+  ) {}
 
   @Get()
   getHello(): string {
@@ -14,11 +18,6 @@ export class AppController {
   @UseGuards(LocalAuthGuard)
   @Post('auth/login')
   async login(@Request() req) {
-    return req.user;
-  }
-
-  @Get('auth/login')
-  async loginTest() {
-    return 'login!';
+    return this.authService.login(req.user)
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,8 @@ import { AppModule } from './app.module';
 declare const module: any;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   await app.listen(3000);
-  app.enableCors();
 
   if (module.hot) {
     module.hot.accept();
