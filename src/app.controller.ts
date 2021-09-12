a0d67b51747f3c3b20e233336d013434cde597d4
slpwlkr <shadezmax41@gmail.com>
2021-09-12 19:35:27 +0800

    add basic user auth module

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
+import { LocalAuthGuard } from './auth/local-auth.guard';
 
 @Controller()
 export class AppController {
@@ -8,5 +9,16 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @UseGuards(LocalAuthGuard)
+  @Post('auth/login')
+  async login(@Request() req) {
+    return req.user;
+  }
+
+  @Get('auth/login')
+  async loginTest() {
+    return 'login!';
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,8 +2,23 @@ import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 
+// 仅测试用
+export type User = any;
 @Injectable()
 export class UsersService {
+  private readonly users = [
+    {
+      userId: 1,
+      username: 'user1',
+      password: '111111',
+    },
+    {
+      userId: 2,
+      username: 'user2',
+      password: '222222',
+    },
+  ];
+
   create(createUserDto: CreateUserDto) {
     return 'This action adds a new user!';
   }
@@ -22,5 +37,9 @@ export class UsersService {
 
   remove(id: number) {
     return `This action removes a #${id} user!`;
+  }
+
+  async findOneByUsername(username: string): Promise<User | undefined> {
+    return this.users.find(user => user.username === username);
   }
 }
